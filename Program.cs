fc533fd69f84798e67a6c1760616991672b2efa2
Kumar Sauarbh <37681828+itsjha@users.noreply.github.com>
2018-03-23 08:59:14 +0000

    Create Program.cs

--- Program.cs
+++ Program.cs
@@ -102,8 +102,8 @@ namespace PopulateBinaryTree
             }
         }
   
-    //class BinarySearchTree
-    //{
+    //    class BinarySearchTree
+    //   {
     //    static void Main(string[] args)
     //    {
     //         Node root = null;
@@ -117,51 +117,20 @@ namespace PopulateBinaryTree
  
     //         Random random = new Random(); 
  
- 
-    //         Stopwatch watch = Stopwatch.StartNew(); 
- 
- 
     //         for (int i = 0; i < SIZE; i++) 
     //         { 
     //             a[i] = random.Next(10000); 
     //         } 
  
  
-    //         watch.Stop(); 
- 
- 
-    //         Console.WriteLine("Done. Took {0} seconds", (double)watch.ElapsedMilliseconds / 1000.0); 
-    //         Console.WriteLine(); 
     //         Console.WriteLine("Filling the tree with {0} nodes...", SIZE); 
- 
- 
-    //         watch = Stopwatch.StartNew(); 
  
  
     //         for (int i = 0; i < SIZE; i++) 
     //         { 
     //             root = bst.insert(root, a[i]); 
     //         } 
- 
- 
-    //         watch.Stop(); 
- 
-    //         Console.WriteLine("Done. Took {0} seconds", (double)watch.ElapsedMilliseconds / 1000.0); 
-    //         Console.WriteLine(); 
-    //         Console.WriteLine("Traversing all {0} nodes in tree...", SIZE); 
- 
- 
-    //         watch = Stopwatch.StartNew(); 
- 
- 
-    //         bst.traverse(root); 
- 
- 
-    //         watch.Stop(); 
- 
-    //         Console.WriteLine("Done. Took {0} seconds", (double)watch.ElapsedMilliseconds / 1000.0); 
-    //         Console.WriteLine(); 
- 
+
     //         Console.ReadKey(); 
 
     //    }
